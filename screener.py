21edb5661ce521010289ceff2f6cdd443d9dddab
danielorf <danielorf@gmail.com>
2017-02-23 22:38:01 -0800

    Added missing comments

--- screener.py
+++ screener.py
@@ -6,16 +6,13 @@ import time
 import urllib.error
 
 
-#good_list = []
 result_list = []
-n_days_ago = 200
-date_at_n_days_ago = datetime.now() - timedelta(days=n_days_ago)
 
-#sec_list = nyse + amex + nasdaq
-sec_list = amex
+n_days_ago = 200  # Start stock history returned
+date_at_n_days_ago = datetime.now() - timedelta(days=n_days_ago)
+sec_list = amex  # Other options: nasdaq, nyse, the_rest
 
 count = 0
-
 for sec in sec_list:
     if '^' not in sec and '.' not in sec:
         print(sec)
@@ -38,25 +35,24 @@ for sec in sec_list:
 
         try:
             sec_hist = security.get_historical(str(date_at_n_days_ago.date()), str(datetime.now().date()))
-            sec_hist_df = pd.DataFrame(sec_hist)
+            sec_hist_df = pd.DataFrame(sec_hist)  # Convert stock history dict to pandas dataframe
             sec_hist_df.Close = sec_hist_df.Close.astype(float)  # Ordered by latest first
-            close_stdev = sec_hist_df.Close.std()
-            price = float(security.get_price())
-            fifty_day_average = float(security.get_50day_moving_avg())
-
-            sec_close_price_array = sec_hist_df.Close.values
-            fifteen_day_average = sec_close_price_array[:12].mean()
-
-            week_to_week_trend = sec_close_price_array[:6].mean()/sec_close_price_array[7:13].mean()-1
-
-            two_hundred_day_average = float(security.get_200day_moving_avg())
-            eps_now = float(security.get_EPS_estimate_current_year())
-            eps_next_year = float(security.get_EPS_estimate_next_year())
+            close_stdev = sec_hist_df.Close.std()  # Standard deviation of stock over returned time
+            price = float(security.get_price())  # Current price
+            fifty_day_average = float(security.get_50day_moving_avg())  # 50 day simple moving average
+            sec_close_price_array = sec_hist_df.Close.values  # Array of close prices over returned time
+            fifteen_day_average = sec_close_price_array[:12].mean()  # 15 day simple moving average
+            week_to_week_trend = sec_close_price_array[:6].mean()\
+                                 /sec_close_price_array[7:13].mean()-1  # Price trend between the previous week and the
+                                                                        # week prior, '>0' indicates improving price
+            two_hundred_day_average = float(security.get_200day_moving_avg())  # 200 day simple moving average
+            eps_now = float(security.get_EPS_estimate_current_year())  # Earnings per share now
+            eps_next_year = float(security.get_EPS_estimate_next_year())  # Earnings per share next year if available
             fifty_two_week_low = float(security.get_year_low())
             fifty_two_week_high = float(security.get_year_high())
             price_earnings_ratio = float(security.get_price_earnings_ratio())
 
-
+            '''Below is list of conditional checks designed to only return undervalued, but improving stocks'''
             if ((price < (two_hundred_day_average - 2 * close_stdev))  # Below bollinger band minimum @ 200 day, 2 stdev
                 and (eps_next_year / eps_now > 1.025)  # Checking for forecasted EPS growth of greater than 10%
                 and (fifty_day_average < two_hundred_day_average)  # 50 day MA should be less than 200 day, discounted
@@ -65,12 +61,11 @@ for sec in sec_list:
                 and (price_earnings_ratio > 0) and (price_earnings_ratio < 30)  # reasonable p/e range
                 and ((fifty_two_week_high - price) / (price - fifty_two_week_low) > 2)):  # Price closer to 52 week low
 
-                #good_list.append(sec)
                 result_list.append({'Symbol': sec, 'Price': price, 'SMA15': fifteen_day_average,
                                     'SMA50': fifty_day_average, 'SMA200': two_hundred_day_average, 'EPS': eps_now,
                                     'EPS_NextYearEst': eps_next_year, 'EPS_Ratio': eps_next_year / eps_now,
                                     '52WeekLow': fifty_two_week_low, '52WeekHigh': fifty_two_week_high,
-                                    'p_e_ratio': price_earnings_ratio})
+                                    'p_e_ratio': price_earnings_ratio})  # Add passing stocks to the result_list
         except TypeError as e5:
             print('Exception! TypeError: ', e5.args)
             continue
@@ -86,5 +81,4 @@ for sec in sec_list:
 
     time.sleep(0.05)
 
-#print(good_list)
 print(result_list)
